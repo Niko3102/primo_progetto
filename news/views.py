33ffd0dacd2733e36716fc3ac567fa19fafc678b
Niko3102 <niko.itisvittuone@gmail.com>
2020-11-18 13:38:22 +0100

    fine L17

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Articolo, Giornalista
 
 def home(request):
@@ -7,3 +7,8 @@ def home(request):
     context = {"articoli": articoli, "giornalisti": giornalisti}
     print(context)
     return render(request, "homepage2.html", context)
+
+def articoloDetailView(request, pk):
+    articolo = get_object_or_404(Articolo, pk=pk)
+    context = {"articolo": articolo}
+    return render(request, "articolo_detail.html", context)
